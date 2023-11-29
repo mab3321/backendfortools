11916ad3eb1ed7d913de8c0e9cc0b434367470f5
mab <mab@mab.com>
2023-11-29 14:44:14 +0500

    Bsic Structure Ready.

--- app.py
+++ app.py
@@ -193,4 +193,4 @@ def return_files_tut():
         return str(e)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True,host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
