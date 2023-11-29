96592ff8e2225c8193367b3d9dc68b98738fe9f4
mab <mab@mab.com>
2023-11-29 18:00:32 +0500

    Added Google Cred.json

--- main.py
+++ main.py
@@ -8,16 +8,17 @@ from flask import Flask, send_file, request
 from flask_cors import CORS
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
-app = Flask(__name__)
-CORS(app)
-# The route() function of the Flask class is a decorator, 
-# which tells the application which URL should call 
-# the associated function.
-@app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-    return 'Hello World'
- 
-# main driver function
-if __name__ == "__main__":
-    app.run(debug=False,host='0.0.0.0', port=8080)
+def run_app():
+    app = Flask(__name__)
+    CORS(app)
+    # The route() function of the Flask class is a decorator, 
+    # which tells the application which URL should call 
+    # the associated function.
+    @app.route('/')
+    # ‘/’ URL is bound with hello_world() function.
+    def hello_world():
+        return 'Hello World'
+    
+    # main driver function
+    if __name__ == "__main__":
+        app.run(debug=False,host='0.0.0.0', port=8080)
